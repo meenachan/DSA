0e1d8ac1aa2204c931fe930b54cb11da63aa7ec9
meena <mcquityppie3@gmail.com>
2021-09-20 20:09:38 +0530

    Minimize the tower height difference

--- MinTowerHeighDiff/minTowerHeightDiff.java
+++ MinTowerHeighDiff/minTowerHeightDiff.java
@@ -1,3 +1,12 @@
+/*
+Minimize the maximum difference between the heights
+Difficulty Level : Hard
+Given heights of n towers and a value k. 
+We need to either increase or decrease the height of every tower by k (only once) where k > 0. 
+The task is to minimize the difference
+ between the heights of the longest and the shortest tower after modifications and output this difference.
+
+*/
 import java.util.Arrays;
 
 public class minTowerHeightDiff{
@@ -9,12 +18,15 @@ public class minTowerHeightDiff{
                 System.out.println("Minimum Tower height difference is: " +getMinDiff(a,n,k));
     }
   public static  int getMinDiff(int[] arr, int n, int k) {
+    //   sort the array to obtain the smallest and tallest tower easily
     Arrays.sort(arr);
-    
+    //calcute the current max difference in tower heights
     int curdiff = arr[n-1]-arr[0];
+    //modifing operation : incrementing the height of small towers and decrementing the heights of tall towers
     int small = arr[0]+k;
     int tall = arr[n-1]-k;
     int min , max;
+    //determining the max and min heights 
     for(int i =0; i<n-1;i++){
         min = Math.min(small,arr[i+1]-k);
         max = Math.max(tall,arr[i]+k);
